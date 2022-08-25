8b5565998b4f9a9795dad6b71683826435df1da6
mohini-patil-dev <mohini.patil.dev@gmail.com>
2022-08-25 07:59:22 +0530

    give types to user filter

--- apps/auth/src/users/users.service.ts
+++ apps/auth/src/users/users.service.ts
@@ -2,13 +2,12 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User, UserDocument } from '../mongodb';
-
+import { IFindUserFilter } from '../types/user.service.types';
 @Injectable()
 export class UsersService {
   constructor(@InjectModel(User.name) private UserModel: Model<UserDocument>) {}
 
-  //TODO: give types here
-  async findOneUser(filter: any): Promise<UserDocument> {
+  async findOneUser(filter: IFindUserFilter): Promise<UserDocument> {
     return this.UserModel.findOne(filter);
   }
 }
